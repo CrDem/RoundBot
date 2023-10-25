e61a1115e0b55016d89246cb7193a399be63ebcd
Anton_Kuznetsov <anvakuznetsov@edu.hse.ru>
2023-10-25 15:39:01 +0300

    added editor

---
+++ editor.py
@@ -0,0 +1,24 @@
+import os
+import ffmpeg
+
+def video_to_video_note(floc, nfloc):
+    probe = ffmpeg.probe(floc)
+    for stream in probe["streams"]:
+        if stream["codec_type"]=="video":
+            w = stream['width']
+            h = stream['height']
+            l = stream['duration']
+    if float(l)>60:
+        return 'toolong'
+    s = min(w,h)
+    if w>h:
+        x=(w-h)//2
+        y=0
+    if w==h:
+        x=0
+        y=0
+    if w<h:
+        x=0
+        y=(h-w)//2
+    os.system(f"ffmpeg -y -i {floc} -vf crop={s}:{s}:{x}:{y},scale=384:384,setsar=1:1 {nfloc}")
+    return True
